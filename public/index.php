0aa9585ec9733fbda029a8b64bafd6277128a572
Yesbol Amantay <esbolarai@gmail.com>
2024-10-13 13:19:17 +0500

    feat: complete 1 task

--- public/index.php
+++ public/index.php
@@ -1,3 +1,47 @@
-<?php
+<?php declare(strict_types=1);
 
-phpinfo();
+require './PDOConnection.php';
+
+function dd($var): void {
+    echo '<pre>';
+    print_r($var);
+    echo '<pre>';
+    die;
+}
+
+function parseClientId(string $clientIdsFromQuery): array {
+    $clientIds = explode(',', $clientIdsFromQuery);
+    if ($clientIds) {
+        return array_map('intval', $clientIds);
+    }
+    return [];
+
+}
+
+function getClientOrders(array $clients): array {
+    $pdo = PDOConnection::getInstance();
+    $placeholders = implode(',', array_map(function ($idx) {
+        return ":id$idx";
+    }, array_keys($clients)));
+    $stmt = $pdo->prepare("
+        SELECT * FROM user_order uo
+        INNER JOIN products p ON p.id = uo.product_id 
+        WHERE uo.user_id IN ($placeholders)
+        ORDER BY p.title, p.price DESC
+    ");
+    foreach ($clients as $idx => $clientId) {
+        $stmt->bindParam(":id$idx", $clients[$idx], PDO::PARAM_INT);
+    }
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
+if (!empty($_GET['client_ids'])) {
+    $clientIds = parseClientId($_GET['client_ids']);
+    if (!$clientIds) return;
+    $orders = getClientOrders($clientIds);
+    header('Content-Type: application/json');
+    echo json_encode($orders);
+}
